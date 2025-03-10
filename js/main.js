70eb66973a1fed404884c84cf15cac9fb43141ab
leigaray <garay.lei.edu@gmail.com>
2025-03-10 18:30:13 -0400

    added other js files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-document.getElementById("loadData").addEventListener("click", function() {
-    fetch("./mock-data.json")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("output").innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch(error => console.error("Error fetching mock data:", error));
-});
+$(function() {
+	// Nice Select
+	$('select').niceSelect();
+})
